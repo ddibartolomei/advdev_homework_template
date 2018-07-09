a5079b9413d85574ae099bac42d09df63ce853d2
Daniele Di Bartolomei <daniele.dibartolomei@redhat.com>
2018-07-09 02:38:32 +0200

    CICD and Dev env

--- Infrastructure/bin/cleanup.sh
+++ Infrastructure/bin/cleanup.sh
@@ -8,8 +8,8 @@ fi
 
 GUID=$1
 echo "Removing all Homework Projects for GUID=$GUID"
-oc delete project $GUID-nexus
-oc delete project $GUID-sonarqube
-oc delete project $GUID-jenkins
+#oc delete project $GUID-nexus
+#oc delete project $GUID-sonarqube
+#oc delete project $GUID-jenkins
 oc delete project $GUID-parks-dev
-oc delete project $GUID-parks-prod
+#oc delete project $GUID-parks-prod
