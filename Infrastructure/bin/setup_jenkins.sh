50264083cdcf8ef4bd6c1eb63bee23191e354e8c
Daniele Di Bartolomei <daniele.dibartolomei@redhat.com>
2018-07-12 03:49:39 +0200

    Fixed maven slave

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -35,11 +35,10 @@ oc set resources dc jenkins --requests=memory=2Gi,cpu=1 --limits=memory=4Gi,cpu=
 oc rollout resume dc jenkins -n ${GUID}-jenkins
 
 # Push Jenkins slave maven image with skopeo support
-systemctl enable docker
-systemctl start docker
-docker build ./Infrastructure/extra -t docker-registry-default.apps.${CLUSTER}/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9
-skopeo copy --dest-tls-verify=false --dest-creds=$(oc whoami):$(oc whoami -t) docker-daemon:docker-registry-default.apps.${CLUSTER}/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9 docker://docker-registry-default.apps.${CLUSTER}/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9
+#oc process -f ./Infrastructure/templates/maven-slave-template.yaml --param GUID=${GUID} --param CLUSTER=${CLUSTER} | oc create -f - -n ${GUID}-jenkins
+oc new-build -D $'FROM docker.io/openshift/jenkins-slave-maven-centos7:v3.9\nUSER root\nRUN yum -y install skopeo && yum clean all\nUSER 1001' --name=jenkins-slave-maven-appdev -n ${GUID}-jenkins
 
+#docker build ./Infrastructure/extra -t docker-registry-default.apps.${CLUSTER}/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9
 #docker login -u $(oc whoami) -p $(oc whoami -t) docker-registry-default.apps.${CLUSTER}
 #docker push docker-registry-default.apps.${CLUSTER}/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9
 
